7c9c639932bd94beb5efbb0f829358e46b444817
yudhalexa <101029025+yudhalexa@users.noreply.github.com>
2025-03-30 21:52:19 +0700

    Step 3 - Request necessary permissions

--- app/src/main/java/ac/id/umn/cameraxapp/MainActivity.kt
+++ app/src/main/java/ac/id/umn/cameraxapp/MainActivity.kt
@@ -72,7 +72,9 @@ class MainActivity : AppCompatActivity() {
 
     private fun startCamera() {}
 
-    private fun requestPermissions() {}
+    private fun requestPermissions() {
+        activityResultLauncher.launch(REQUIRED_PERMISSIONS)
+    }
 
     private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
         ContextCompat.checkSelfPermission(
@@ -97,4 +99,24 @@ class MainActivity : AppCompatActivity() {
                 }
             }.toTypedArray()
     }
+
+    private val activityResultLauncher =
+        registerForActivityResult(
+            ActivityResultContracts.RequestMultiplePermissions())
+        { permissions ->
+            // Handle Permission granted/rejected
+            var permissionGranted = true
+            permissions.entries.forEach {
+                if (it.key in REQUIRED_PERMISSIONS && it.value == false)
+                    permissionGranted = false
+            }
+            if (!permissionGranted) {
+                Toast.makeText(baseContext,
+                    "Permission request denied",
+                    Toast.LENGTH_SHORT).show()
+            } else {
+                startCamera()
+            }
+        }
+
 }
